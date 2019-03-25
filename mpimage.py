b21f2b976a44932ad3d8e1c3dd573f7549d35737
jumper <jumper@jumperdebian.ten.thomsonreuters.com>
2019-03-25 18:39:52 -0400

    changed from queues to shared dict

--- mpimage.py
+++ mpimage.py
@@ -37,25 +37,38 @@ def colorfunc2(ix,iy):
 
 
 
-def renderimg(width, height, offx, offy, rgbfunction,aqueue):
-    print("starting subrender..."+str((width, height, offx, offy, rgbfunction)))
+def renderimg(width, height, offx, offy, rgbfunction,returndictionary,index):
+    print("starting subrender..."+str((width, height, offx, offy, rgbfunction.__name__)))
     img = Image.new('RGB', (width, height), color = (0, 0, 0))
     pimg = img.load()
     for yy in range(height):
         for xx in range(width):
             pimg[xx,yy]=rgbfunction(xx+offx,yy+offy)
-    print("finished subrender..."+str((width, height, offx, offy, rgbfunction)))
-    aqueue.put(img)
-    return img
+    #aqueue.put(img)
+    returndictionary[index]=(offx,offy,img)
+    print("finished subrender..."+str((width, height, offx, offy, rgbfunction.__name__)))
 
-def mprenderbyfunc(mainwide, mainhigh, colorfunction,xoffset=0,yoffset=0,procs=2):
+def mprenderbyfunc(mainwide, mainhigh, colorfunction,xoffset=0,yoffset=0,procs=2,perprocsplit=10):
+    """
+    create an image, dimensions mainwide x mainhigh
+    colorfunction takes values x,y and returns r,g,b
+    xoffset,yoffset are added to x,y coordinates in the image to align the function
+    procs is the number of processes that will be loaded (default 2)
+    perprocsplit is used to divide the image more. Different x,y can have different loads
+      by splitting the image further we are less likely to stick one core with a lot of
+      processing and another with little to do.
+    """
     mainimg = Image.new('RGB', (mainwide, mainhigh), color = (255, 0, 255))
 
+
+    mp_manager=smp.Manager()
+    subimgdict=mp_manager.dict()
     queuelist=[]
     processlist=[]
 
     splitx=1;
-    splity=procs;
+    splity=procs*perprocsplit;
+    prcidx=0
     for spx in range(splitx):
         for spy in range(splity):
             suboffx=round((mainwide*spx)/splitx)
@@ -64,35 +77,43 @@ def mprenderbyfunc(mainwide, mainhigh, colorfunction,xoffset=0,yoffset=0,procs=2
             suboffyplus1=round((mainhigh*(spy+1))/splity)
             subwide=suboffxplus1-suboffx
             subhigh=suboffyplus1-suboffy
-            childq=smp.Queue()
-            iprocess=smp.Process(target=renderimg,args=(subwide,subhigh,suboffx+xoffset,suboffy+yoffset,colorfunction,childq,))
-            queuelist.append((suboffx, suboffy, childq))
+            childq=smp.SimpleQueue()
+            iprocess=smp.Process(target=renderimg,args=(subwide,subhigh,suboffx+xoffset,suboffy+yoffset,colorfunction,subimgdict,prcidx,))
+            prcidx+=1
             processlist.append(iprocess)
-    a=0
-    for eachprocess in processlist:
-        a+=1
-        print("starting process "+str(a))
-        eachprocess.start()
     
     complete=0
-    while complete==0:
+    while complete==0: #loop until all process are complete
         complete=1
-        for  prcs in processlist:
-            if (prcs.is_alive==True):
-                prcs.join(timeout=0)
-                print("process "+str(prcs.pid)+"  "+str(prcs.is_alive()))
+        running=0
+        pcount=0
+        for  prcs in processlist:  #for each process
+            pcount+=1
+            #print("process "+str(pcount)+" "+str(prcs.is_alive())+"::"+str(prcs.exitcode))
+            palive=prcs.is_alive()
+            if (palive==True):     #if it's alive, we're not done, and one is running
                 complete=0
-        time.sleep(0.01)
+                running+=1
+                prcs.join() #this seems to be necessary to trigger a process into finishing
+        #print("running:"+str(running))
+        if running < procs:
+            for prcs in processlist:
+                if (prcs.is_alive()==False) and (prcs.exitcode==None): #if process isn't alive and has not exited, it never ran
+                    prcs.start()                                       #so run it, increment running, and obvs, we're not done
+                    running+=1
+                    complete=0
+                    if running == procs:                               #stop running processes if we hit our max process limit
+                        break
+        time.sleep(0.001) #we can skip sleep, but python is wasteful on these checks...
     
-    for eachq in queuelist:
-        sx,sy,cldq=eachq
-        subimage=cldq.get()
-        mainimg.paste(subimage,(sx,sy))
+    for eachentry in subimgdict.values():
+        sx,sy,img=eachentry
+        mainimg.paste(img,(sx-xoffset,sy-yoffset))
 
     return mainimg
 
 def main():
-    mprenderbyfunc(600,600,colorfunc2).save("./test.png")
+    mprenderbyfunc(1600,1600,colorfunc1,procs=2,perprocsplit=1).save("./test.png")
 
-if __name__=="__main__":
+if __name__ == '__main__':
     main()
