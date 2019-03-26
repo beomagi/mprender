86932234b8578b7f151df52044f26b4efb69593c
beomagi <beomagi@gmail.com>
2019-03-25 22:27:59 -0400

    switched to using map instead of custom job scheduler

--- mpimage_v1.py
+++ mpimage_v1.py
@@ -37,17 +37,16 @@ def colorfunc2(ix,iy):
 
 
 
-#def renderimg(width, height, offx, offy, rgbfunction):
-def renderimg(params):
-    width, height, offx, offy, rgbfunction = params
+def renderimg(width, height, offx, offy, rgbfunction,returndictionary,index):
     print("starting subrender..."+str((width, height, offx, offy, rgbfunction.__name__)))
     img = Image.new('RGB', (width, height), color = (0, 0, 0))
     pimg = img.load()
     for yy in range(height):
         for xx in range(width):
             pimg[xx,yy]=rgbfunction(xx+offx,yy+offy)
+    #aqueue.put(img)
+    returndictionary[index]=(offx,offy,img)
     print("finished subrender..."+str((width, height, offx, offy, rgbfunction.__name__)))
-    return((offx,offy,img))
 
 def mprenderbyfunc(mainwide, mainhigh, colorfunction,xoffset=0,yoffset=0,procs=2,perprocsplit=10):
     """
@@ -61,9 +60,15 @@ def mprenderbyfunc(mainwide, mainhigh, colorfunction,xoffset=0,yoffset=0,procs=2
     """
     mainimg = Image.new('RGB', (mainwide, mainhigh), color = (255, 0, 255))
 
-    processargs=[]
+
+    mp_manager=smp.Manager()
+    subimgdict=mp_manager.dict()
+    queuelist=[]
+    processlist=[]
+
     splitx=1;
     splity=procs*perprocsplit;
+    prcidx=0
     for spx in range(splitx):
         for spy in range(splity):
             suboffx=round((mainwide*spx)/splitx)
@@ -73,11 +78,35 @@ def mprenderbyfunc(mainwide, mainhigh, colorfunction,xoffset=0,yoffset=0,procs=2
             subwide=suboffxplus1-suboffx
             subhigh=suboffyplus1-suboffy
             childq=smp.SimpleQueue()
-            processargs.append((subwide,subhigh,suboffx+xoffset,suboffy+yoffset,colorfunction))
-    renderpool=smp.Pool(procs)
-    imgreturn=renderpool.map(renderimg,processargs)
-
-    for eachentry in imgreturn:
+            iprocess=smp.Process(target=renderimg,args=(subwide,subhigh,suboffx+xoffset,suboffy+yoffset,colorfunction,subimgdict,prcidx,))
+            prcidx+=1
+            processlist.append(iprocess)
+    
+    complete=0
+    while complete==0: #loop until all process are complete
+        complete=1
+        running=0
+        pcount=0
+        for  prcs in processlist:  #for each process
+            pcount+=1
+            #print("process "+str(pcount)+" "+str(prcs.is_alive())+"::"+str(prcs.exitcode))
+            palive=prcs.is_alive()
+            if (palive==True):     #if it's alive, we're not done, and one is running
+                complete=0
+                running+=1
+                prcs.join() #this seems to be necessary to trigger a process into finishing
+        #print("running:"+str(running))
+        if running < procs:
+            for prcs in processlist:
+                if (prcs.is_alive()==False) and (prcs.exitcode==None): #if process isn't alive and has not exited, it never ran
+                    prcs.start()                                       #so run it, increment running, and obvs, we're not done
+                    running+=1
+                    complete=0
+                    if running == procs:                               #stop running processes if we hit our max process limit
+                        break
+        time.sleep(0.001) #we can skip sleep, but python is wasteful on these checks...
+    
+    for eachentry in subimgdict.values():
         sx,sy,img=eachentry
         mainimg.paste(img,(sx-xoffset,sy-yoffset))
 
